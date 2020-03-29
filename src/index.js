7aa2d03fe02021e640bb258c3d9a9455a09b1a64
Mônica Santos <monicasantosdev@gmail.com>
2020-03-28 22:36:33 -0300

    initial structure

---
+++ src/index.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { StyleSheet, View, Text, StatusBar } from 'react-native';
+
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+});
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Hello!</Text>
+      </View>
+    </>
+  );
+};
+
+export default App;
